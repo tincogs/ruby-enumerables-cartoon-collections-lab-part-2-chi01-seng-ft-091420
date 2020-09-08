e83e038bf1a97c4aa3a0187fbcec6981b9822c94
Tom ODay <tomoday91@gmail.com>
2020-09-08 18:58:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find{|string| string == valid_calls[string]}
+  planeteer_calls.find{|string| valid_calls.include(string)}
 end
